c532c75f2e6a095394961b7bedd755e1af72f45b
rif8treyerl9ip <rif8treyerl9ip@users.noreply.github.com>
2022-03-05 15:48:46 +0900

    add

---
+++ shellscrpt/20220305.bat
@@ -0,0 +1,11 @@
+@REM 管理者権限が必要
+
+cd C:\Program Files\WindowsApps\hash化された文字列？\app
+start Slack.exe
+
+cd C:\Users\thyt\AppData\Local\Microsoft\Teams\current
+start Teams.exe
+
+cd C:\Users\thyt\SELF_S\shellscrpt\
+
+exit
---
+++ shellscrpt/20220305_2.bat
@@ -0,0 +1,6 @@
+explorer "C:\Users\thyt\OneDrive\ドキュメント\Office のカスタム テンプレート"
+explorer "C:\Users\thyt\Desktop\TMP\"
+
+start https://www.google.com/?hl=ja
+
+exit
